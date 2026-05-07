ed5077ba6891d9a14c84c927c18a60838165a664
JoseAmaliya <joseamaliyaambarwati2@gmail.com>
2026-05-07 13:02:22 +0700

    Convert index.php to return JSON data
    Removed HTML structure and replaced with JSON response for user data.

--- index.php
+++ index.php
@@ -1,46 +1,15 @@
 <?php
 include 'koneksi.php';
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PHP CRUD Railway</title>
-</head>
-<body>
+header('Content-Type: application/json');
 
-<h2>Tambah Data</h2>
-
-<form method="POST" action="tambah.php">
-    <input type="text" name="nama" placeholder="Nama">
-    <input type="password" name="sandi" placeholder="Sandi">
-    <button type="submit">Simpan</button>
-</form>
-
-<h2>Data Users</h2>
-
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Nama</th>
-        <th>Sandi</th>
-    </tr>
-
-<?php
 $data = mysqli_query($koneksi, "SELECT * FROM users");
 
+$users = [];
+
 while($d = mysqli_fetch_assoc($data)){
+    $users[] = $d;
+}
+
+echo json_encode($users, JSON_PRETTY_PRINT);
 ?>
-
-<tr>
-    <td><?php echo $d['id']; ?></td>
-    <td><?php echo $d['nama']; ?></td>
-    <td><?php echo $d['sandi']; ?></td>
-</tr>
-
-<?php } ?>
-
-</table>
-
-</body>
-</html>
